3fe573b728f97de417d0ecf138ce1cfaa5ed3e17
Petar Radisavljevic <petar.radisav@gmail.com>
2024-01-11 18:13:00 +0100

    Icons and some UI components has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getgoing_flutter/screens/get_going_main.dart';
 
-void main() => runApp(MaterialApp(home: GetGoingMain()));
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: GetGoingMain()));
--- lib/screens/get_going_main.dart
+++ lib/screens/get_going_main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class GetGoingMain extends StatefulWidget {
   const GetGoingMain({super.key});
@@ -10,12 +12,146 @@ class GetGoingMain extends StatefulWidget {
 class _GetGoingMainState extends State<GetGoingMain> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        child: const Text('Hello World'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12.0, 16.0, 16.0, 0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SvgPicture.asset('assets/ic_logo_small.svg',
+                      width: 50.0, height: 50.0),
+                  SvgPicture.asset('assets/ic_light_user.svg',
+                      width: 30.0, height: 40.0),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Last exercise',
+                    style: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {});
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          'View all',
+                          style: TextStyle(
+                              color: Colors.blue[300], fontSize: 18.0),
+                        ),
+                        const SizedBox(width: 4.0),
+                        SvgPicture.asset('assets/ic_light_chevron.svg',
+                            width: 14.0, height: 14.0)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+              child: Container(
+                height: 180.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  gradient: const LinearGradient(
+                      colors: [
+                        Color.fromRGBO(0x20, 0xba, 0xFF, 1.0),
+                        Color.fromRGBO(0x6b, 0xd1, 0xFF, 1.0)
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [0.0, 1.0]),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Center(
+                        child: CircularPercentIndicator(
+                          radius: 60,
+                          lineWidth: 5,
+                          percent: 0.5,
+                          progressColor: Colors.blue[700],
+                          backgroundColor: Colors.white,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 12.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/ic_running_white.svg',
+                                  // Update with the correct path
+                                  width: 30.0,
+                                  height: 24.0,
+                                ),
+                                const Text('50%',
+                                    style: TextStyle(
+                                        fontSize: 16.0, color: Colors.white)),
+                                const Text('Running',
+                                    style: TextStyle(
+                                        fontSize: 14.0, color: Colors.white))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: CircularPercentIndicator(
+                          radius: 58,
+                          lineWidth: 5,
+                          percent: 0.25,
+                          progressColor: Colors.blue[700],
+                          backgroundColor: Colors.white,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 12.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/ic_timer.svg',
+                                  // Update with the correct path
+                                  width: 25.0,
+                                  height: 25.0,
+                                ),
+                                const Text('25%',
+                                    style: TextStyle(
+                                        fontSize: 16.0, color: Colors.white)),
+                                const Text('min',
+                                    style: TextStyle(
+                                        fontSize: 14.0, color: Colors.white))
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
